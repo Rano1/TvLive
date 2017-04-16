e54441c38cc85d3f0ebe52ba92691861a7ffd83c
Will <myfuture@vip.qq.com>
2017-04-16 21:39:36 +0800

    提交斗鱼文档

---
+++ tv/DouYu.py
@@ -0,0 +1,26 @@
+# 斗鱼TV，通过官方API文档
+import socket,json,re,select,time,requests
+
+class DouYuDanMuClient(object):
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.codeLocalToServer = 689
+        self.codeServerToLocal = 690
+        self.gid = -9999
+        self.rid = 16789
+        self.server = {}
+
+    def getInfo(self, roomId):
+        url = 'http://open.douyucdn.cn/api/RoomApi/room/%s' % roomId
+        print(url)
+        data = requests.get(url).json()
+        if data.get('error') == 0:
+            print(data)
+        else:
+            print("请求错误")
+
+
+if (__name__ == '__main__'):
+    roomId = 'wt55kai'
+    danmu = DouYuDanMuClient()
+    danmu.getInfo(roomId)
