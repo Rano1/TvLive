d053f19b18e061fa2cf7c7bd0d1bb5acd85caf48
Will <myfuture@vip.qq.com>
2017-05-30 21:18:58 +0800

    添加Python中的Redis测试

---
+++ sql/RedisTest.py
@@ -0,0 +1,9 @@
+# Redis测试
+import redis
+
+host = '192.168.2.105'
+port = 6379
+# password = ''
+
+r = redis.StrictRedis(host=host, port=port, db=0, password=password)
+r.hset('user', 'name', 'will')
